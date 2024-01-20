12b0f976a19ef6f38cf3f8e9bdb6824f86449ce4
Łukasz Mencel <59538107+vvooki@users.noreply.github.com>
2024-01-20 01:17:48 +0100

    Removed additional comment

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -31,7 +31,6 @@ router.post('/', verifyToken, async (req, res) => {
 });
 
 router.put('/:id', verifyToken, async (req, res) => {
-  console.log('tutaj');
   try {
     const updatedTask = await taskService.updateTask(req.params.id, req.body);
     res.json(updatedTask);
